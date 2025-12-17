8145ed27a43b71e9561b459d58eb1d8f021637d1
MarzooqTechCarrot52 <abdulmarzooq.sultan@techcarrot.ae>
2025-12-17 11:15:44 +0530

    commiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GreetController } from './greet/greet.controller';
-import { HelloController } from './hello/hello.controller';
 import { InventoryModule } from './inventory/inventory.module';
 import { BookModule } from './book/book.module';
 import { AddHeaderMiddleware } from './add.header';
-import path from 'path';
+
 
 @Module({
   imports: [InventoryModule, BookModule],
-  controllers: [AppController, GreetController, HelloController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule{
